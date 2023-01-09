20fab2d01ae939b386dc12059439d959a4fa5bd9
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 19:30:04 +0100

    Fixed the background for template

--- javaScript/myScript.js
+++ javaScript/myScript.js
@@ -1,11 +1,10 @@
 function dropdownNav() {
-    
     if (document.getElementById("nav").style.display == "none"){
-        document.getElementById("dropdownlogo").style.transform = 'rotate(180deg)';
+        // document.getElementById("dropdownlogo").style.transform = 'rotate(180deg)';
         
         document.getElementById("nav").style.display = "block";
     }else{
-        document.getElementById("dropdownlogo").style.transform = 'rotate(0)';
+        // document.getElementById("dropdownlogo").style.transform = 'rotate(0)';
         document.getElementById("nav").style.display = "none";
     }
     
