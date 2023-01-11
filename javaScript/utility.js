a42fbf49b0a8c271fbe0c5c914bb9210de76c235
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 23:59:35 +0100

    Added fixed heights for nav and Header added javascript for perfect resizing main block when nav dropdown + resizing window

--- javaScript/utility.js
+++ javaScript/utility.js
@@ -14,21 +14,35 @@ function dropdownNav() {
     let mainBody = document.getElementById("main-body");
     let iconHamburger = document.getElementById("icon--menu");
 
+
+    mainBody.style.transition = "min-height 300ms 0s ease-in-out";
     if (nav.style.maxHeight == "60px") {
         iconHamburger.style.transform = "rotate(0)";
-        mainBody.style.transition = "min-height 300ms 0s ease-in-out";
+        
         collapseElement(nav);
         mainBody.style.minHeight = "calc(100vh - 114px)";
-        mainBody.style.transition = "none";
-
     } else {
         iconHamburger.style.transform = "rotate(180deg)";
-        mainBody.style.transition = "min-height 300ms 0s ease-in-out";
         expandElement(nav, 60);
         mainBody.style.minHeight = "calc(100vh - 174px)";
-        mainBody.style.transition = "none";
     }
+
+    // The header is 84px height
+    // nav is switching from 0 to 60 px
+    // footer is 30px normally but this can change if the windows get very small
+
+    // a time out is needed here because otherwise the transition doesn't happen
+    setTimeout(function(){mainBody.style.transition = "none";}, 300);
+    // this create a problem however if you spamm the button you get a scrollbar
+    // this needs to be fixed by adding a cooldown on the button [TODO]
+
+    // set a timeout to prevent spamming
+
+
+    
 }
+
+
 
 function toggleDiv(toToggle, maxHeight) {
     if (toToggle.style.maxHeight == "0px") {
@@ -45,6 +59,12 @@ document.getElementById("nav--account").addEventListener("mouseenter", function 
     document.getElementById("nav").style.overflow = 'visible';
 });
 
+// fixes a bug that if the footer got bigger in heigth you would get a scrollbar when it was not needed.
+// works on resizing the window and zooming in and out :)
+window.addEventListener("resize", function(){
+    let total = document.getElementById("main-footer").offsetHeight + document.getElementById("header-and-nav").offsetHeight;
+    document.getElementById("main-body").style.minHeight = "calc(100vh - " + total + "px)";
+})
 
 document.getElementById("drop-down-container").addEventListener("mouseleave", 
 
