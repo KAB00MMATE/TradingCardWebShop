b36896e0f0a617aa9028277c19942a084b9fbb9c
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 23:49:51 +0100

    added a simple adding tag system

--- javaScript/new_offer.js
+++ javaScript/new_offer.js
@@ -2,12 +2,10 @@ let tag_input = document.getElementById("input-line-tag");
 let tag_output = document.getElementById("tags-block");
 
 function addTag(){
-    const new_tag = document.createElement("p");
-    new_tag.appendChild(document.createTextNode(tag_input.value));
-    // tag_input.append(new_tag);
-
-    document.body.insertBefore(new_tag, tag_output);
-
-
-    alert("yo");
+    let text = tag_input.value;
+    let text_node = document.createTextNode(text);
+    let new_tag = document.createElement("div");
+    new_tag.appendChild(text_node);
+    tag_output.appendChild(new_tag);
+    tag_input.value = "";
 }
