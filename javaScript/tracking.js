1cfb96f393a681f562508aa7993fe8e73544ed40
sbakker6 <70519285+sbakker6@users.noreply.github.com>
2023-01-20 00:54:30 +0100

    Documentation enhanced

--- javaScript/tracking.js
+++ javaScript/tracking.js
@@ -14,25 +14,31 @@ const timeElement = document.querySelector("#tracking-stats-time");
 const keyElement = document.querySelector("#tracking-stats-key");
 const charsElement = document.querySelector("#tracking-stats-chars-typed");
 
-const statTrackingElements = document.querySelectorAll("form input:is([type='text'], [type='email'], [type='password'])");
+// NodeList (Immutable!) of elements in the DOM that should keep track of their own stats: keypresses and characters typed
+const statTrackingElements = document.querySelectorAll("form input:is([type='text'], [type='email'], [type='password']");
 
-
-// Text email and password fields should have their own records of the keypresses and the amount of characters they contain.
+// Text, email and password fields should have their own records of the keypresses and the amount of characters they contain.
 const charactersPerElement = {};
 const keyPressesPerInput = {};
-const statDisplayElements = [];
 
-const displayElementSuffix = "__display-stats";
 
+const statDisplayElements = []; // Array of the stat-displaying elements that are dynamically created, so that they all can easily be accessed later on
+
+const displayElementSuffix = "__display-stats"; // Used as suffix to the ID of the HTML element that this displaying element is referring to. 
+
+// Add all necessary eventlisteners and dynamically create new elements for the elements that are supposed to be kept track of
 statTrackingElements.forEach(
     (element) => {
+        // Initialize the object record for this element
         keyPressesPerInput[element.id] = 0;
         charactersPerElement[element.id] = element.value.length;
 
+        // Add event listeners for keypresses (note that keypressed is deprecated and that's why keydown is used) and keychanged 
         element.addEventListener("change", () => { charactersPerElement[element.id] = element.value.length; });
         element.addEventListener("keydown", () => { keyPressesPerInput[element.id]++; });
 
 
+        // Create new elements that are going to display the stats for this element
         const newElement = document.createElement("p");
         newElement.classList.add("stats");
         newElement.innerText = `${element.id}`;
@@ -44,19 +50,10 @@ statTrackingElements.forEach(
 
 
 // Hookup submit button to the functionality
-
-
-
 document.querySelector("#btn-block :nth-child(3)").addEventListener('click', () => {
     updateInfo();
     revealStatsElements();
 });
-
-
-
-
-
-
 
 // Helper functions
 
@@ -85,7 +82,7 @@ function updateInfo() {
     // Set the stats for each input element 
     statDisplayElements.forEach((e) => {
         const parentElementID = e.id.split(displayElementSuffix)[0];
-        e.innerText =   `Keys pressed: ${keyPressesPerInput[parentElementID]} | Characters typed: ${charactersPerElement[parentElementID]}`;
+        e.innerText = `Keys pressed: ${keyPressesPerInput[parentElementID]} | Characters typed: ${charactersPerElement[parentElementID]}`;
     });
 }
 
