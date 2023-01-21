eafa17dbc2e0d9c47ce0c3284eb9175270f35e41
BuildTools <unconfigured@null.spigotmc.org>
2023-01-21 22:37:03 +0100

    regex branch done

--- javaScript/sign_up.js
+++ javaScript/sign_up.js
@@ -1,124 +1,47 @@
-const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-const path = "../documents/dictionary.txt";
+function validate() {
+    const userID = validateUserID();
+    console.log("user ID check: ", userID);
+    const email = validateEMail();
+    console.log("email check: ", email);
+    const PWD = validatePWD();
+    console.log("password check: ", PWD);
+    const name = validateName()
+    console.log("name check: ", name);
+    const country = validateCountry();
+    console.log("country check: ", country);
+    const language = validateLanguage()
+    console.log("language check: ", language);
 
-const dictionary = loadFile(path).split("\n");
 
-function validate(){
-    console.log("user ID check: " + validateUserID());
-    console.log("email check: " + validateEMail());
-    console.log("password check: " + validatePWD());
-    console.log("name check: ", validateName());
-    console.log("country check: ", validateCountry());
-    console.log("gender check: ", validateGender());
-    console.log("language check: ", validateLanguage());
+    const success = userID && email && PWD && name && country && gender && language;
+    if (success){
+        alertInfo();
+    }
+    console.log(success);
+    return success;
 }
 
 function validateUserID() {
-    const min = 5;
-    const max = 12;
-    input_userID_Value = document.getElementById("userID").value;
+    inputUserIdValue = document.getElementById("userID").value;
 
     output = document.getElementById("userID-output");
 
-// checks if the input is empty, this is technically not needed. Because we can just use REQUIRED
-    if (emptyString(input_userID_Value)){
-        console.log("User ID is empty");
-        Incorrect(output, "User ID is empty");
+    // Min length = 5, max = 12
+    const idPattern = /[A-Z].{3,10}[^a-zA-Z]$/
+
+    return validatePattern(idPattern, inputUserIdValue, output, msgFalse = `User ID should be 5-12 characters long, 
+    start with a capital letter and end with a special character or number`);
+}
+
+
+function validatePattern(pattern, text, output, msgTrue = "Looks good!", msgFalse = "Not quite right") {
+    if (!pattern.test(text)) {
+        incorrect(output, msgFalse);
         return false;
     }
 
-// although this is already checked through the form, with minlength="5" and maxlength="12"
-    if (!checkLength(input_userID_Value, min, max)){
-        console.log("Length is incorrect: " + input_userID_Value.length);
-        Incorrect(output, "Length should be between: " + min + " and " + max);
-        return false;
-    }
-    
-    if (!startsWithCapital(input_userID_Value)){
-        console.log("Doesn't start with a capital letter: " + input_userID_Value.charAt(0));
-        Incorrect(output, "Doesn't start with a capital letter");
-        return false;
-    }
-
-    if (!endsWithNumberOrSpecial(input_userID_Value)){
-        console.log("Doesn't end with a special character or number: " + input_userID_Value.charAt(input_userID_Value.length-1));
-        Incorrect(output, "Doesn't end with a special character or number");
-        return false;
-    }
-
-    Correct(output, "User ID is accepted")
+    correct(output, msgTrue);
     return true;
-}
-
-function emptyString(word){
-    if (word.length == 0){
-        return true;
-    }
-    return false;
-}
-
-// check if the length of the word is inbetween min and max, both are included
-function checkLength(word, min, max){
-    return word.length >= min && word.length <= max;
-}
-
-// checks if the length of the word is larger or equal to min
-function checkLength(word, min){
-    return word.length >= min;
-}
-
-// check if the word starts with a capital letter
-function startsWithCapital(word){
-    let letter = word.charAt(0);
-    return isCapital(letter);
-}
-
-// checks if the input is capital letters
-function isCapital(letter){
-    if (!isNaN(letter*69)){
-        return false;
-    }
-    if (letter == letter.toLowerCase()){
-        return false;
-    }
-    if (letter == letter.toUpperCase()){
-        return true;
-    }
-}
-
-// return true if the word or char contains one of the special characters
-function containsSpecialChar(word){
-    return specialChars.test(word)
-}
-
-// return true if word or char contains uppercase letter
-function containsUppercase(word){
-    return /[A-Z]/.test(word);
-}
-
-// return true if word or char contains lowercase letter
-function containsLowercase(word){
-    return /[a-z]/.test(word);
-}
-
-// return true if word or char contains a number
-function containsNumber(word){
-    return /[0-9]/.test(word);
-}
-
-// return true if the word ends with a number or a special character
-function endsWithNumberOrSpecial(word){
-    let letter = word.charAt(word.length-1);
-
-    if(containsSpecialChar(letter)){
-        return true;
-    }
-
-    if (!isNaN(letter * 69)){
-        return true;
-    }
-
-    return false;
 }
 
 function validatePWD() {
@@ -130,185 +53,70 @@ function validatePWD() {
 
     output = document.getElementById("password_user-output");
 
-// checks if both passwords are the same
-    if (input_PWD_Value !== input_PWD2_Value){
+    // checks if both passwords are the same || Technically not RegEx, but we feel like this is a vital part of the page working as it should. 
+    if (input_PWD_Value !== input_PWD2_Value) {
         console.log("Passwords are not the same");
         console.log("PWD1: " + input_PWD_Value);
         console.log("PWD2: " + input_PWD2_Value);
-        Incorrect(output, "Passwords are not the same");
+        incorrect(output, "Passwords are not the same");
         return false;
     }
 
-    
-// checks if the input is empty, this is technically not needed. Because we can just use REQUIRED
-    if (emptyString(input_PWD_Value)){
-        console.log("Password is empty");
-        Incorrect(output, "Password is empty");
-        return false;
-    }
+    const pattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{12,}/;
 
-// although this is already checked through the form, with minlength="12"
-    if (!checkLength(input_PWD_Value, min)){
-        console.log("Length is incorrect: " + input_PWD_Value.length);
-        Incorrect(output, "Too short, min: " + min);
-        return false;
-    }else{
-        // checks if the input under the recommended length
-        if (!checkLength(input_PWD_Value, recommended)){
-            console.log("Password is a bit short, recommended length: " + recommended);
-        }
-    }
 
-// checks if there are lowercase letters
-    if (!containsLowercase(input_PWD_Value)){
-        console.log("Doesn't contain lowercase letters");
-        Incorrect(output, "Doesn't contain a lowercase letter");
-        return false;
-    }
-
-// checks if there are uppercase letters
-    if (!containsUppercase(input_PWD_Value)){
-        console.log("Doesn't contain uppercase letters");
-        Incorrect(output, "Doesn't contain uppercase letter");
-        return false;
-    }
-
-// checks if there are numbers
-    if (!containsNumber(input_PWD_Value)){
-        console.log("Doesn't contain numbers");
-        Incorrect(output, "Doesn't contain a number");
-        return false;
-    }
-
-// checks if there are special characters
-    if (!containsSpecialChar(input_PWD_Value)){
-        console.log("Doesn't contain special characters");
-        Incorrect(output, "Doesn't contain a special character");
-        return false;
-    }
-
-// checks if the password is a known word. this a bit weird because numbers are not in the dictionary
-    if (dictionary.includes(input_PWD_Value.toLowerCase())){
-        console.log("This password is just a normal word");
-        Incorrect(output, "Password has a word in it");
-        return false;
-    }
-
-    return true;
+    return validatePattern(pattern, input_PWD_Value, output, msgFalse = `Password should be at least 12 characters long, 
+    and consist of lowercase and uppercase letters, numbers and special characters`);
 }
 
-function validateName(){
+function validateName() {
     first_name = document.getElementById("fname").value;
     last_name = document.getElementById("lname").value;
     output_first = document.getElementById("fname-output");
     output_last = document.getElementById("lname-output");
-    output_last.innerHTML = "yoyoyoyoy";
 
-// check if the input is not empty. Redunded because the form has REQUIRED
-    if (emptyString(first_name)){
-        console.log("empty first name");
-        Incorrect(output_first, "Empty first name");
-        return false;
-    }
+    const pattern = /[A-Z]\w*/;
 
-    if (emptyString(last_name)){
-        console.log("empty last name");
-        Incorrect(output_last, "Empty last name");
-        return false;
-    }
-
-// this system is not bulletproof. If you enter a character that is not in the list, the input will get accepted.
-    if (containsNumber(first_name)){
-        console.log("first name contains number");
-        Incorrect(output_first, "First name contains a number");
-        return false;
-    }
-
-    if (containsNumber(last_name)){
-        console.log("last name contains number");
-        Incorrect(output_last, "Last name contains a number");
-        return false;
-    }
-
-    if (containsSpecialChar(first_name)){
-        console.log("first name contains special character");
-        Incorrect(output_first, "First name contains a special character");
-        return false;
-    }
-
-    if (containsSpecialChar(last_name)){
-        console.log("last name contains special character");
-        Incorrect(output_last, "Last name contains a special character");
-        return false;
-    }
-
-    Correct(output_first, "First name accepted");
-    Correct(output_last, "Last name accepted");
-    return true;
+    const firstNameValid = validatePattern(pattern, first_name, output_first);
+    const lastNameValid = validatePattern(pattern, last_name, output_last);
+    return firstNameValid && lastNameValid;
 }
 
 // function for checking if a country is choosen.
 // this function is technically not really needed because we can just set the first option as default
-function validateCountry(){
+function validateCountry() {
     input_country = document.getElementById("country").value;
+    output = document.getElementById("country-output");
+    pattern = /.+/; // Note that the default option is "", so any character will indicate a non-default option chosen.
 
-    if (input_country == "default"){
-        console.log("country not choosen");
-        return false
-    }
-
-    return true;
-}
-
-// this check is a bit not needed, because in the form we use CHECKED, meaning always one is already checked
-function validateGender(){
-    input_male = document.getElementById("male").checked;
-    input_female = document.getElementById("female").checked;
-    input_other = document.getElementById("other").checked;
-    
-    return input_male || input_female || input_other;
+    return validatePattern(pattern, input_country, output);
 }
 
 // function for checking if a language is choosen.
 // this function is technically not really needed because we can just set the first option as default
-function validateLanguage(){
+function validateLanguage() {
     input = document.getElementById("language").value;
+    output = document.getElementById("language-output");
 
-    if (input == "default"){
-        console.log("language not choosen");
-        return false;
-    }
-
-    return true;
+    pattern = /.+/; // Note that the default option is "", so any character will indicate a non-default option chosen.
+    return validatePattern(pattern, input_country, output);
 }
 
-function validateEMail(){
+function validateEMail() {
     input = document.getElementById("email_user").value;
     output = document.getElementById("email_user-output");
-    
-    if (emptyString(input)){
-        console.log("Email empty");
-        Incorrect(output, "E-mail field is empty");
-        return false;
-    }
-
-    if (!/@/.test(input)){
-        console.log("Email doesn't contain @");
-        Incorrect(output, "E-mail field doesn't contain @");
-        return false;
-    }
-
-    return true;
+    const pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}/
+    return validatePattern(pattern, input, output);
 }
 
 // changes the output field to the correct style
-function Correct(node, text){
+function correct(node, text) {
     node.innerHTML = text;
     node.className = "input-correct";
 }
 
 // changes the output field to the incorrect style
-function Incorrect(node, text){
+function incorrect(node, text) {
     node.innerHTML = text;
     node.className = "input-wrong";
 }
@@ -320,12 +128,12 @@ function loadFile(filePath) {
     req.send();
     var result = "nothing :)";
     if (req.status == 200) {
-      result = req.responseText;
+        result = req.responseText;
     }
     return result;
 }
 
-function alertInfo(){
+function alertInfo() {
     user_id = document.getElementById("userID").value;
     email = document.getElementById("email_user").value;
     PWD = document.getElementById("password_user").value;
